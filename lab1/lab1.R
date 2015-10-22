0658af6e9eca5400ec3aa60d777dd00fd476bb9c
Gonzalo Moya <gemoya@inf.utfsm.cl>
2015-10-22 18:12:49 -0300

    half script

--- lab1/lab1.R
+++ lab1/lab1.R
@@ -5,3 +5,16 @@ cv_mpg<-(sd(mpg)/mean(mpg))
 cv_cylinders<-(sd(cylynders)/mean(cylinders))
 cv_displacement<-(sd(displacement)/mean(displacement))
 cv_horsepower<-(sd(horsepower)/mean(horsepower))
+cv_weight<-(sd(weight)/mean(weight))
+cv_acceleration<-(sd(acceleration)/mean(acceleration))
+cv_model_year<-(sd(model_year)/mean(model_year))
+
+hist(model_year)
+
+
+cov(displacement, cylinders)/(sd(displacement)*sd(cylinders))
+cov(cylinders, acceleration)/(sd(cylinders)*sd(acceleration))
+
+plot(cylinders,horsepower)
+plot(model_year,horsepower)
+
